64a9ada54cf58565bdad12dde98b4600b7a768d5
Roberto Tienda <rohatiro@gmail.com>
2023-05-26 11:40:32 -0600

    Trabajando con Guards

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -85,8 +85,16 @@ export class AuthService {
     };
   }
 
-  findAll() {
-    return `This action returns all auth`;
+  findAll(): Promise<User[]> {
+    return this.userModel.find();
+  }
+
+  async findUserById(id: string) {
+    const user = await this.userModel.findById(id);
+
+    const { password, ...rest} = user.toJSON();
+
+    return rest;
   }
 
   findOne(id: number) {
